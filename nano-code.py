13c76ed1c8632bcdda37ff94aed1f88604bb90a3
Rajesh Pavan Pothukuchi <110812838+rajeshpavanpothukuchi@users.noreply.github.com>
2022-08-17 13:27:03 +0530

    Update nano-code.py

--- nano-code.py
+++ nano-code.py
@@ -8,16 +8,6 @@ from scipy.spatial import distance_matrix
 import time
 sys.setrecursionlimit(10**6)
 
-def dist(x1,y1,z1,x2,y2,z2):
-	if abs(x1-x2)>140:
-		dx=(x1-x2)-280
-	if abs(y1-y2)>140:
-		dy=(y1-y2)-280
-	if abs(z1-z2)>140:
-		dz=(z1-z2)-280
-	return ((dx)**2+(dy)**2+(dz)**2)**0.5	
-
-
 class Graph:
 	def __init__(self,V): 
 		self.V = V 
@@ -49,10 +39,8 @@ class Graph:
 				temp = [] 
 				cc.append(self.DFSUtil(temp, v, visited)) 
 		return cc 
-
-
 def check_graph(lines, arr,crr):	
-	g = Graph(1000);
+	g = Graph(#Ng*Np*N#);
 	a=np.array([list(map(float,i.split(','))) for i in lines])
 	y=distance_matrix(a,a,2)
 	y=(9.5>=y)&(y>0)
@@ -60,65 +48,47 @@ def check_graph(lines, arr,crr):
 		for j in range(len(lines)):
 			if(j>i):
 				if(y[i][j]==True):
-					g.addEdge(i,j) 
-	# for i in range(len(lines)):
-	# 	for j in range(i+1,len(lines)):
-	# 		point1=lines[i]
-	# 		point2=lines[j]
-	# 		x = [float(o) for o in point1.split(',')]
-	# 		y=	[float(o) for o in point2.split(',')]
-	# 		# print(x,y)
-
-	# 		if(dist(x[0],x[1],x[2],y[0],y[1],y[2])<=11):
-	# 			# print(dist(x[0],x[1],x[2],y[0],y[1],y[2]))
-	# 			g.addEdge(i,j)
-	 			
+					g.addEdge(i,j) 	 			
 	cc = g.connectedComponents() 
-	# print("Following are connected components") 
-	# print(cc)
-
-	# b=[]
 	for i in range(len(cc)):
 		comp = cc[i]
 		cors = ''
 		for j in comp:
-			if len(g.adj[j])<5 or (len(cc[i])/10)%2>0:
+			if len(g.adj[j])<#Np# or (len(cc[i])/#2*Np#)%2>0:
 				cors = 'string'
 				# arr[len(cc[i])]+=1
 				break
 			else:
 				cors = 'cycle'
 				# crr[len(cc[i])]+=1
-		print(len(cc[i]), cors,file=open("res-520-0.5-30.txt", "a"))
+		print(len(cc[i]), cors,file=open("result.txt", "a"))
 		if cors=='string':
 			arr[len(cc[i])]+=1
 		else:
 			crr[len(cc[i])]+=1
 if __name__=="__main__":
-	fp = open('520-0.5-30.txt','r')
+	fp = open('input.txt','r')
 	lines = fp.read().split('\n\n\n')
 	fp.close()
-	arr = [0 for j in range(1001)]
-	crr = [0 for j in range(1001)]
+	arr = [0 for j in range(#(Ng*Np*N)+1#)]
+	crr = [0 for j in range(#(Ng*Np*N)+1#)]
 	a=0
 	for i in lines:
 		if len(i)>2:
 			a=a+1
 			start = time.time()
 			print("Following are connected components", a)
-			print("Following are connected components", a,file=open("res-520-0.5-30.txt", "a"))
+			print("Following are connected components", a,file=open("result.txt", "a"))
 			check_graph(i.split('\n'),arr,crr)
 			end=time.time()
 			#print(end - start)
-	print("overall Frequency",file=open("res-520-0.5-30.txt", "a"))	
-	print("String",file=open("res-520-0.5-30.txt", "a"))	
-	for i in range(1,1001):
+	print("overall Frequency",file=open("result.txt", "a"))	
+	print("String",file=open("result.txt", "a"))	
+	for i in range(1,#(Ng*Np*N)+1#):
 		#print("overall Frequency")
 		if arr[i]>0:
-	 		print(i,':', arr[i],file=open("res-520-0.5-30.txt", "a"))
-	print("Cycle",file=open("res-520-0.5-30.txt", "a")) 	
-
-	for j in range(1,1001): 
+	 		print(i,':', arr[i],file=open("result.txt", "a"))
+	print("Cycle",file=open("result.txt", "a")) 	
+	for j in range(1,#(Ng*Np*N)+1#): 
 		if crr[j]>0:	
-	 		print(j,':', crr[j],file=open("res-520-0.5-30.txt", "a"))
-
+	 		print(j,':', crr[j],file=open("result.txt", "a"))
